f96d7ecff2ad521ed672f6047764091f3ef4c4d5
Mapleaf <xytyl@qq.com>
2016-08-29 09:10:16 +0800

    AppMarket

--- ApplicationMarket/app/src/main/java/com/appmarket/mapleaf/applicationmarket/bean/SubjectInfo.java
+++
@@ -1,9 +0,0 @@
-package com.appmarket.mapleaf.applicationmarket.bean;
-
-/**
- * Created by Mapleaf on 2016/7/11.
- */
-public class SubjectInfo {
-    public String des;
-    public String iconUrl;
-}
